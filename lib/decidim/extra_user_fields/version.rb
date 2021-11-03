3b87cf4a6e6a74056e3dc631a55076a5c846340d
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2021-11-03 21:40:35 +0100

    Upgrade decidim_version and module version to use the same

--- lib/decidim/extra_user_fields/version.rb
+++ lib/decidim/extra_user_fields/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-extra_user_fields version.
   module ExtraUserFields
     def self.version
-      "0.1"
+      "0.25.1"
     end
 
     def self.decidim_version
-      ">= 0.25.0"
+      "0.25.1"
     end
   end
 end
