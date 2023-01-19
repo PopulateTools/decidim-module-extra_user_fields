2eab462420fee94f33a742f2f92dba88985d4463
Francisco Bolívar <francisco.bolivar@nazaries.com>
2023-01-19 13:51:33 +0100

    Update to 0.26.4

--- lib/decidim/extra_user_fields/version.rb
+++ lib/decidim/extra_user_fields/version.rb
@@ -4,11 +4,11 @@ module Decidim
   # This holds the decidim-extra_user_fields version.
   module ExtraUserFields
     def self.version
-      "0.26.2"
+      "0.26.4"
     end
 
     def self.decidim_version
-      "0.26.2"
+      "0.26.4"
     end
   end
 end
