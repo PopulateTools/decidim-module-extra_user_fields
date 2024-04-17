680dfbc1aa8b999b1379ffa5e90bf0da8ea4bc20
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2024-04-17 13:51:39 +0200

    Avoid duplicated labels

--- lib/decidim/extra_user_fields/form_builder_methods.rb
+++ lib/decidim/extra_user_fields/form_builder_methods.rb
@@ -9,7 +9,7 @@ module Decidim
         label_text = label_for(name) if label_text.blank?
 
         template = ""
-        template += label(name, label_text + required_for_attribute(name)) if options.fetch(:label, true)
+        template += (label_text + required_for_attribute(name)) if options.fetch(:label, true)
         template += country_select(name)
         template.html_safe
       end
