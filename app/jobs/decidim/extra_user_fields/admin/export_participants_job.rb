ed3ce427d9728f6703cf69febcbc3c73988f7195
stephanie rousset <strabon@club-internet.fr>
2025-01-20 16:26:18 +0100

    feat: add job

---
+++ app/jobs/decidim/extra_user_fields/admin/export_participants_job.rb
@@ -0,0 +1,17 @@
+# frozen_string_literal = true
+
+module Decidim
+  module ExtraUserFields
+    module Admin
+      class ExportParticipantsJob < ApplicationJob
+        queue_as :exports
+
+        def perform(organization, user, format)
+          collection = organization.users.not_deleted
+          export_data = Decidim::Exporters.find_exporter(format).new(collection, Decidim::ExtraUserFields::UserExportSerializer).export
+          ExportMailer.export(user, "participants", export_data).deliver_now
+        end
+      end
+    end
+  end
+end
