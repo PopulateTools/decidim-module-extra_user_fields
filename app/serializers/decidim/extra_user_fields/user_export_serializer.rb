a9d32b52a690e5391320237d55da4d925876498a
stephanie rousset <strabon@club-internet.fr>
2025-01-20 16:24:57 +0100

    feat: update serializer

--- app/serializers/decidim/extra_user_fields/user_export_serializer.rb
+++ app/serializers/decidim/extra_user_fields/user_export_serializer.rb
@@ -13,7 +13,7 @@ module Decidim
       def extra_user_fields
         extended_data = resource.extended_data.symbolize_keys
 
-        [:gender, :country, :postal_code, :date_of_birth, :phone_number, :location].index_with do |key|
+        [:gender, :country, :postal_code, :date_of_birth, :phone_number, :location, :underage, :statutory_representative_email].index_with do |key|
           extended_data[key]
         end
       end
@@ -27,6 +27,8 @@ module Decidim
           :date_of_birth,
           :phone_number,
           :location,
+          :underage,
+          :statutory_representative_email
           # Block ExtraUserFields AddExtraField
 
           # EndBlock
