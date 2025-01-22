e84f1def177ce4e636e7a42e205d5ffc8b2bc876
stephanie rousset <strabon@club-internet.fr>
2025-01-22 11:54:12 +0100

    test: another update of options for webdriver

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -10,10 +10,8 @@ require "decidim/dev/test/base_spec_helper"
 
 Capybara.register_driver :headless_chrome do |app|
   options = Selenium::WebDriver::Chrome::Options.new
+  options.args << "--headless=new"
   options.args << "--no-sandbox"
-  options.args << "--headless"
-  options.args << "--disable-dev-shm-usage"
-  options.args << "--remote-debugging-pipe"
   options.args << if ENV["BIG_SCREEN_SIZE"].present?
                     "--window-size=1920,3000"
                   else
