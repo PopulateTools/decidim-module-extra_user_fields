203fd82fb372c993de82aa6d1870261498fd0648
Eduardo Martinez Echevarria <eduardomech@gmail.com>
2025-05-29 10:36:42 +0200

    Serialize interests field using translations

--- app/serializers/decidim/extra_user_fields/user_export_serializer.rb
+++ app/serializers/decidim/extra_user_fields/user_export_serializer.rb
@@ -7,12 +7,10 @@ module Decidim
       # Public: Exports a hash with the serialized data for the user including
       # extra user fields
       def serialize
-        super.merge(blocking_data).merge(extra_user_fields)
+        super.merge(blocking_data).merge(extra_user_fields).merge(processed_extra_user_fields)
       end
 
       def extra_user_fields
-        extended_data = resource.extended_data.symbolize_keys
-
         [:gender, :country, :postal_code, :date_of_birth, :phone_number, :location, :underage, :statutory_representative_email].index_with do |key|
           extended_data[key]
         end
@@ -40,6 +38,20 @@ module Decidim
 
           # EndBlock
         ]
+      end
+
+      def extended_data
+        @extended_data ||= resource.extended_data.symbolize_keys
+      end
+
+      def processed_extra_user_fields
+        { interests: translated_interests(extended_data[:interests]) }
+      end
+
+      def translated_interests(keys)
+        return if keys.blank?
+
+        keys.map { |key| I18n.t(key, scope: "decidim.extra_user_fields.interests") }.join(", ")
       end
     end
   end
